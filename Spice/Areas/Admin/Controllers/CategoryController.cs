a22bf40c33f4df16fb59d767ab8fe98b692310f8
Ben Spark <bhrugen1990@gmail.com>
2018-12-06 15:27:51 -0600

    Section 5 - Edit Category Get Action

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -50,5 +50,22 @@ namespace Spice.Areas.Admin.Controllers
             return View(category);
         }
 
+
+        //GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if(category==null)
+            {
+                return NotFound();
+            }
+            return View(category);
+            
+        }
+
     }
 }
