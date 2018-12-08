3a29a32ecf9e06add7b4cbc0070e3239c96757b8
Ben Spark <bhrugen1990@gmail.com>
2018-12-08 13:39:04 -0600

    Section 6 - Extension Methods

---
+++ Spice/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Extensions
+{
+    public static class IEnumerableExtension
+    {
+        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this IEnumerable<T> items, int selectedValue)
+        {
+            return from item in items
+                   select new SelectListItem
+                   {
+                       Text = item.GetPropertyValue("Name"),
+                       Value = item.GetPropertyValue("Id"),
+                       Selected = item.GetPropertyValue("Id").Equals(selectedValue.ToString())
+                   };
+        }
+
+    }
+}
---
+++ Spice/Extensions/ReflectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Extensions
+{
+    public static class ReflectionExtension
+    {
+        public static string GetPropertyValue<T>(this T item, string propertyName)
+        {
+            return item.GetType().GetProperty(propertyName).GetValue(item, null).ToString();
+        }
+    }
+}
