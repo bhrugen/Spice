9bbdf424e6a1720bf15b391446883a9f487b62d4
Ben Spark <bhrugen1990@gmail.com>
2018-12-08 11:17:53 -0600

    Section 5 - Delete Category Get Action

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -81,5 +81,22 @@ namespace Spice.Areas.Admin.Controllers
             return View(category);
         }
 
+
+
+        //GET - DELETE
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
     }
 }
