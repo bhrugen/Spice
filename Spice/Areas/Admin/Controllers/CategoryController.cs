a2870a7eb15424ca65c99c55e34d26c5539c8ecf
Ben Spark <bhrugen1990@gmail.com>
2018-12-08 11:51:30 -0600

    Assignment 1 - Category Details

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -113,5 +113,22 @@ namespace Spice.Areas.Admin.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        //GET - DETAILS
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
     }
 }
