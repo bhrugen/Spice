2fcaee756a21f1ce42fd2e4460bf9cc698877032
Ben Spark <bhrugen1990@gmail.com>
2018-12-08 13:19:53 -0600

    Section 6 - SubCategory And Category View Model

---
+++ Spice/Models/ViewModels/SubCategoryAndCategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models.ViewModels
+{
+    public class SubCategoryAndCategoryViewModel
+    {
+        public IEnumerable<Category> CategoryList { get; set; }
+        public SubCategory SubCategory { get; set; }
+        public List<string> SubCategoryList { get; set; }
+        public string StatusMessage { get; set; }
+    }
+}
