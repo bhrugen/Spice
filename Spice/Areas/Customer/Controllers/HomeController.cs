e9171881dea6a5b279d77ab581b0fcf88f1c6de2
Ben Spark <bhrugen1990@gmail.com>
2018-12-09 11:47:21 -0600

    Section 9 - Home Index Action

--- Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Areas/Customer/Controllers/HomeController.cs
@@ -4,16 +4,34 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
 using Spice.Models;
+using Spice.Models.ViewModels;
 
 namespace Spice.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            IndexViewModel IndexVM = new IndexViewModel()
+            {
+                MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).ToListAsync(),
+                Category = await _db.Category.ToListAsync(),
+                Coupon = await _db.Coupon.Where(c => c.IsActive == true).ToListAsync()
+
+            };
+            return View(IndexVM);
         }
 
         public IActionResult Privacy()
