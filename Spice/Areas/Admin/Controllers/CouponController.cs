84a33a112224a7b4af8377f16d4414aaa69d2a05
Ben Spark <bhrugen1990@gmail.com>
2018-12-09 01:38:24 -0600

    Section 8 - Create Coupons POST

--- Spice/Areas/Admin/Controllers/CouponController.cs
+++ Spice/Areas/Admin/Controllers/CouponController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models;
 
 namespace Spice.Areas.Admin.Controllers
 {
@@ -26,6 +28,33 @@ namespace Spice.Areas.Admin.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupon coupons)
+        {
+            if(ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if(files.Count>0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    coupons.Picture = p1;
+                }
+                _db.Coupon.Add(coupons);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coupons);
         }
     }
 }
