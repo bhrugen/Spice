e76950011dd085bbe73ccbf0596cbe536ec5c154
Ben Spark <bhrugen1990@gmail.com>
2018-12-09 01:20:44 -0600

    Section 8 - Coupon Index View And Action

---
+++ Spice/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouponController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CouponController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Coupon.ToListAsync());
+        }
+    }
+}
