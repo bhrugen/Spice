13bba9c7c2c678e0cd7e210597bb036b8cbdcfd3
Ben Spark <bhrugen1990@gmail.com>
2018-12-09 11:40:39 -0600

    Section 9 - Index View Model

---
+++ Spice/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<MenuItem> MenuItem { get; set; }
+        public IEnumerable<Category> Category { get; set; }
+        public IEnumerable<Coupon> Coupon { get; set; }
+
+    }
+}
