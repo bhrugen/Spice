bd38786130f8264b5b166a5d2318745539d3c6ba
Ben Spark <bhrugen1990@gmail.com>
2018-12-15 13:36:36 -0600

    Section 12 - Shopping Cart Index Part 1

--- Spice/Areas/Customer/Controllers/CartController.cs
+++ Spice/Areas/Customer/Controllers/CartController.cs
@@ -11,6 +11,7 @@ using Spice.Utility;
 
 namespace Spice.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
