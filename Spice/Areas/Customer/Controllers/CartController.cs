ba2dcfb99e688ca494f22a570ba84151cebf7e24
Ben Spark <bhrugen1990@gmail.com>
2018-12-15 15:44:34 -0600

    Section 12 - Remove Coupon

--- Spice/Areas/Customer/Controllers/CartController.cs
+++ Spice/Areas/Customer/Controllers/CartController.cs
@@ -79,5 +79,13 @@ namespace Spice.Areas.Customer.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult RemoveCoupon()
+        {
+            
+            HttpContext.Session.SetString(SD.ssCouponCode,string.Empty);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
