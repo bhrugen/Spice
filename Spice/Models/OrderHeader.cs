afddc232845dff42734aacee3e6d701979f3bb2c
Ben Spark <bhrugen1990@gmail.com>
2018-12-15 12:37:09 -0600

    Section 12 - Order Header Model

---
+++ Spice/Models/OrderHeader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        public double   OrderTotalOriginal { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [Display(Name ="Order Total")]
+        public double OrderTotal { get; set; }
+
+        [Required]
+        [Display(Name = "Pickup Time")]
+        public DateTime PickUpTime { get; set; }
+
+        [Required]
+        [NotMapped]
+        public DateTime PickUpDate { get; set; }
+
+       [Display(Name ="Coupon Code")]
+        public string CouponCode { get; set; }
+        public double CouponCodeDiscount { get; set; }
+        public string Status { get; set; }
+        public string PaymentStatus { get; set; }
+        public string Comments { get; set; }
+
+
+        [Display(Name = "Pickup Name")]
+        public string PickupName { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        public string TransactionId { get; set; }
+
+
+
+
+    }
+}
