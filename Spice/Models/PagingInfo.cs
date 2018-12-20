e5ce6ed60fd13ac0a00d22f28ef4bbb856179a9d
Ben Spark <bhrugen1990@gmail.com>
2018-12-19 21:42:50 -0600

    Section 13 - PagingInfo Model

---
+++ Spice/Models/PagingInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItem { get; set; }
+
+        public int ItemsPerPage { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int totalPage => (int)Math.Ceiling((decimal)TotalItem / ItemsPerPage);
+
+        public string urlParam { get; set; }
+    }
+}
