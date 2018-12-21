19176d4eb20482e7292504e40ec2f8b032e19e96
Ben Spark <bhrugen1990@gmail.com>
2018-12-21 17:41:43 -0600

    Section 15 - View Component Action Method

---
+++ Spice/ViewComponenets/UserNameViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Spice.ViewComponenets
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserNameViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var userFromDb = await _db.ApplicationUser.FirstOrDefaultAsync(u => u.Id == claims.Value);
+
+            return View(userFromDb);
+        }
+
+    }
+}
