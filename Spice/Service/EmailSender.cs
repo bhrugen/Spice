13a5317f848b547fd8fcdee852c31eba39eb55c3
Ben Spark <bhrugen1990@gmail.com>
2019-01-02 19:58:23 -0600

    Section 16 - Configure Settings for Email Part 2

--- Spice/Service/EmailSender.cs
+++ Spice/Service/EmailSender.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,38 @@ namespace Spice.Service
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public EmailOptions Options { get; set; }
+
+        public EmailSender(IOptions<EmailOptions> emailOptions)
         {
-            throw new NotImplementedException();
+            Options = emailOptions.Value;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Execute(Options.SendGridKey, subject, message, email);
+        }
+
+        private Task Execute(string sendGridKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(sendGridKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("admin@spice.com", "Spice Restaurant"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+            try
+            {
+                return client.SendEmailAsync(msg);
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return null;
         }
     }
 }
