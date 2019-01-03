ebebcf1466410e541a3d934f1af7aa143b352e4e
Ben Spark <bhrugen1990@gmail.com>
2019-01-02 19:42:29 -0600

    Section 16 - Send Grid API Key

---
+++ Spice/Service/EmailOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Service
+{
+    public class EmailOptions
+    {
+        public string SendGridKey { get; set; }
+    }
+}
