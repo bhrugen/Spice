6f68075c7f7c21b00b2a62d42cd165f316870e02
Ben Spark <bhrugen1990@gmail.com>
2019-01-04 14:23:04 -0600

    Section 17 - Seed Database Part 1

--- Spice/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Spice/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -97,23 +97,7 @@ namespace Spice.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    if(!await _roleManager.RoleExistsAsync(SD.ManagerUser))
-                    {
-                        await _roleManager.CreateAsync(new IdentityRole(SD.ManagerUser));
-                    }
-                    if (!await _roleManager.RoleExistsAsync(SD.CustomerEndUser))
-                    {
-                        await _roleManager.CreateAsync(new IdentityRole(SD.CustomerEndUser));
-                    }
-                    if (!await _roleManager.RoleExistsAsync(SD.KitchenUser))
-                    {
-                        await _roleManager.CreateAsync(new IdentityRole(SD.KitchenUser));
-                    }
-                    if (!await _roleManager.RoleExistsAsync(SD.FrontDeskUser))
-                    {
-                        await _roleManager.CreateAsync(new IdentityRole(SD.FrontDeskUser));
-                    }
-
+                   
                     if(role==SD.KitchenUser)
                     {
                         await _userManager.AddToRoleAsync(user, SD.KitchenUser);
